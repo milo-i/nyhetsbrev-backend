b23131c2fb978f981580b305cbfde50a344163cd
Igor Milosevic <igor.milosevic1988@gmail.com>
2021-04-19 21:37:43 +0200

    creates post router

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const cors = require('cors');
+router.use(cors());
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  res.send('hello');
+
+  res.send('Hello users');
+
+
+});
+
+
+
+router.post('/new', function (req, res, next) {
+  console.log(req.body);
+
+  res.json('ny användare sparad');
+
+
 });
 
 module.exports = router;
