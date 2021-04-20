d67943788edb5f311260291516a3adbde05077a2
Igor Milosevic <igor.milosevic1988@gmail.com>
2021-04-20 14:33:54 +0200

    user input is given a random key, user password is encrypted. all user input received at client is stored at mongodb atlas

---
+++ models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+ id: {
+  type: String,
+  required: true
+ },
+ userName: {
+  type: String,
+  required: true
+ },
+ userPassword: {
+  type: String,
+  required: true
+ },
+ subscription: {
+  type: String,
+  required: true
+ }
+}, { timestamps: true });
+
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
--- routes/users.js
+++ routes/users.js
@@ -4,29 +4,42 @@ const cors = require('cors');
 router.use(cors());
 const mongodb = require('mongodb');
 const mongoose = require('mongoose');
+const User = require('../models/user');
+const randomKey = require('random-key');
+const CryptoJS = require('crypto-js');
 
 
 
 // Connecting to my mongodb
 const dbURI = 'mongodb+srv://igor2021:igor2021@cluster0.hnt8d.mongodb.net/nyhetsbrev?retryWrites=true&w=majority';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((result) => console.log('uppkopplad mot databasen')) // Kan behöva lägga in listen på servern så att man inte gör anrop innan db laddas
+  .then((result) => console.log('uppkopplad mot databasen')) // Kan behöva lägga in listen på servern istället för console.log så att man inte gör anrop innan db laddas
   .catch((err) => console.log(err));
+
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
 
   res.send('Hello users');
-
-
 });
-
-
 
 router.post('/new', function (req, res, next) {
   console.log(req.body);
 
-  res.json('ny användare sparad');
+  let userPass = req.body.userPassword;
+  let encryptPass = CryptoJS.AES.encrypt(userPass, 'SecretKey123').toString();
+
+  const user = new User({
+    id: randomKey.generate(),
+    userName: req.body.userEmail,
+    userPassword: encryptPass,
+    subscription: req.body.subscription
+  });
+
+  user.save().then((result) => {
+    res.json('ny användare sparad');
+  })
+    .catch((err) => { console.log(err) })
 
 
 });
