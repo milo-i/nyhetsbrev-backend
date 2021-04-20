b1d4cbf0949e4d7b16f21f120f8e59b4f55dc4d9
Igor Milosevic <igor.milosevic1988@gmail.com>
2021-04-20 12:59:18 +0200

    creates connection to db. installed mongodb and mongoose packages

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,16 @@ var express = require('express');
 var router = express.Router();
 const cors = require('cors');
 router.use(cors());
+const mongodb = require('mongodb');
+const mongoose = require('mongoose');
+
+
+
+// Connecting to my mongodb
+const dbURI = 'mongodb+srv://igor2021:igor2021@cluster0.hnt8d.mongodb.net/nyhetsbrev?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => console.log('uppkopplad mot databasen')) // Kan behöva lägga in listen på servern så att man inte gör anrop innan db laddas
+  .catch((err) => console.log(err));
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
